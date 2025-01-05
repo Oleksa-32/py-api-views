bd4bf2ac644cd0f8e50db59f1501a9ff87f4a954
user <graban2253@gmail.com>
2025-01-05 05:55:47 +0200

    Finished

--- cinema/urls.py
+++ cinema/urls.py
@@ -10,10 +10,12 @@ cinema_hall_list = CinemaHallViewSet.as_view(
 )
 
 cinema_hall_detail = CinemaHallViewSet.as_view(
-    actions={"get": "retrieve",
-             "put": "update",
-             "patch": "partial_update",
-             "delete": "destroy"}
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy"
+    }
 )
 
 router = routers.DefaultRouter()
@@ -22,11 +24,11 @@ router.register("movies", MovieViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("genres/", GenreList.as_view(), name="genre-list"),
-    path("genres/<int:pk>", GenreDetail.as_view(), name="genre-detail"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
-    path("actors/<int:pk>", ActorDetail.as_view(), name="actor-detail"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
-    path("cinema_halls/<int:pk>",
+    path("cinema_halls/<int:pk>/",
          cinema_hall_detail,
          name="cinema-hall-detail"),
 ]
