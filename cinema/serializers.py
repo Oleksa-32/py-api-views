fdfa55026f8d996f4d3cc66e268a9fa245371a10
user <graban2253@gmail.com>
2025-01-05 05:36:36 +0200

    Finished

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -17,6 +17,9 @@ class MovieSerializer(serializers.Serializer):
         instance.description = validated_data.get(
             "description", instance.description
         )
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get("description",
+                                                  instance.description)
         instance.duration = validated_data.get("duration", instance.duration)
 
         instance.save()
